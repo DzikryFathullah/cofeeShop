2f959c9b9fe1a7ca2b8d63a548bd17dad42df194
DzikryFathullah <dzizee.fathullah@gmail.com>
2018-10-25 21:47:28 +0700

    progress cofee CRUD done

--- routes/cofeeRouter.js
+++ routes/cofeeRouter.js
@@ -5,6 +5,9 @@ const cofeeController = require('../controller/cofeeController.js')
 cofeeRouter.get('/',cofeeController.show)
 cofeeRouter.get('/add',cofeeController.showAdd)
 cofeeRouter.post('/add',cofeeController.add)
+cofeeRouter.get('/edit/:id',cofeeController.showEdit)
+cofeeRouter.post('/edit/:id',cofeeController.edit)
+cofeeRouter.get('/delete/:id',cofeeController.delete)
 
 
 module.exports = cofeeRouter
